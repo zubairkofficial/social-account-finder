6646b1e97f0f335b006af3980e7a88e1dc954829
unknown <thezubarikhan.developer@gmail.com>
2025-04-11 18:22:02 +0500

    linked search complete

--- src/profile-search/profile-search.controller.ts
+++ src/profile-search/profile-search.controller.ts
@@ -1,18 +1,37 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { ProfileSearchService } from './profile-search.service';
+import { ProfileSearchDto } from './dto/profile.dto';
 
 @Controller('profile-search')
 export class ProfileSearchController {
-  constructor(private readonly profileSearchService: ProfileSearchService) {}
+  constructor(private readonly profileSearchService: ProfileSearchService) { }
+
+
 
   @Get('find')
   async findProfile(
     @Query('name') name: string,
     @Query('email') email: string,
+    @Query('company') company: string,
     @Query('city') city: string,
     @Query('state') state: string,
+    @Query('country') country: string,
+    @Query('address1') address1: string,
+    @Query('address2') address2: string,
   ) {
+    const data: ProfileSearchDto = {
+      name,
+      email,
+      company,
+      address: {
+        city,
+        state,
+        country,
+        address1,
+        address2
+      }
+    }
     // return await this.profileSearchService.findLinkedProfile(name, email, city, state);
-    return await this.profileSearchService.findLinkedProfile();
+    return await this.profileSearchService.getLinkedProfile(data);
   }
 }
--- src/profile-search/profile-search.service.ts
+++ src/profile-search/profile-search.service.ts
@@ -1,6 +1,8 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { ProfileSearchDto } from './dto/profile.dto';
+import { StateEnum } from 'src/utils/states.enum';
+import { CountryEnum } from 'src/utils/coutries.enum';
 
 export enum ConfidenceCount {
   FULL_NAME = 50,
@@ -19,8 +21,13 @@ export enum AddressConfidenceCount {
 interface PersonData {
   displayName: string,
   location: string,
-  companyName: string
+  companyName: string,
+  link?: string
+}
 
+interface ResultCompare {
+  confidence: number,
+  linkedInURL: string
 }
 
 @Injectable()
@@ -101,85 +108,180 @@ export class ProfileSearchService {
   }
 
   async getLinkedProfile(profileSearchDto: ProfileSearchDto) {
-    let confidence_on_email = 0;
-    let confidence_on_name = 0;
-    let confidence_on_link = 0;
-    try {
+    console.log("profile Data", profileSearchDto)
+    let email_linked_data: ResultCompare;
 
-      if (profileSearchDto.email) {
+    let name_linked_data: ResultCompare[] = [] //confidence, url, 
+
+    let url_linked_data: ResultCompare[] = [] //confidence, url
+    //for url email: linkedInUrl, || name: navigationUrl,
+    //for company: email: positions.positionHistory[].company.companyName, ||   name: no company,  || link: experiences[].title, experiences[].subtitle
+    if (profileSearchDto.email) {
+      const options = {
+        method: 'POST',
+        url: 'https://linkedin-data-scraper.p.rapidapi.com/email_to_linkedin_profile',
+        headers: {
+          'x-rapidapi-key': 'bcfab707b6msh3b13e094ad1bf26p19071ajsn41f8ae0ee227',
+          'x-rapidapi-host': 'linkedin-data-scraper.p.rapidapi.com',
+          'Content-Type': 'application/json'
+        },
+        data: {
+          email: profileSearchDto.email
+        }
+      };
+
+      try {
+        const response = await axios.request(options);
+        if (response?.data?.success) {
+
+          let companyName: string = `${response?.data?.person_data?.companyName}, `
+          companyName = companyName + response?.data?.person_data?.positions?.positionHistory?.map((position: any) => position?.company?.companyName).join(', ')
+
+
+          const displayName: string = `${response?.data?.person_data?.displayName}`
+
+          const location: string = `${response?.data?.person_data?.location}`
+          const link: string = `${response?.data?.linkedinUrl}`
+
+          const personData: PersonData = {
+            displayName,
+            companyName,
+            location,
+          }
+
+          const data: any = this.calculateConfidence(profileSearchDto, personData)
+
+          email_linked_data = {
+            confidence: data,
+            linkedInURL: link
+          }
+          console.log("Email:", email_linked_data);
+        }
+
+        setTimeout(() => {
+          console.log("Link item");
+        }, 1 * 1000); // delay increases with index
+      } catch (error) {
+        setTimeout(() => {
+          console.log("Link item");
+        }, 1 * 1000); // delay increases with index
+        console.error(error.message);
+      }
+    }
+    console.log("came here")
+    if (profileSearchDto.name) {
+      console.log("camer here 1")
+      const options = {
+        method: 'POST',
+        url: 'https://linkedin-data-scraper.p.rapidapi.com/search_person',
+        headers: {
+          'x-rapidapi-key': 'bcfab707b6msh3b13e094ad1bf26p19071ajsn41f8ae0ee227',
+          'x-rapidapi-host': 'linkedin-data-scraper.p.rapidapi.com',
+          'Content-Type': 'application/json'
+        },
+        data: {
+          keywords: `${profileSearchDto.name}`,
+          // keywords: `${profileSearchDto.name}, ${profileSearchDto.company}, ${profileSearchDto.address?.city}, ${StateEnum[`${profileSearchDto.address?.state}`] !== "" ? StateEnum[`${profileSearchDto.address?.state}`] : `${profileSearchDto.address?.state}`}, ${CountryEnum[`${profileSearchDto.address?.country}`] !== "" ? CountryEnum[`${profileSearchDto.address?.country}`] : `${profileSearchDto.address?.country}`}`,
+          geoUrns: '',
+          count: 10
+        }
+      };
+
+      try {
+
+        const response = await axios.request(options);
+        if (response?.data?.success) {
+
+
+          response?.data?.response?.forEach((person_data: any) => {
+            const displayName: string = `${person_data?.fullName}`
+            const companyName: string = `${person_data?.primarySubtitle}`
+            const location: string = `${person_data?.secondarySubtitle}`
+
+            const navigateURL = person_data?.navigationUrl.trim() || ""
+            const indexOfQueryStart = navigateURL.indexOf('?')
+
+            const link = navigateURL.slice(0, indexOfQueryStart)
+
+            const personData: PersonData = {
+              displayName,
+              companyName,
+              location,
+            }
+
+            const data: any = this.calculateConfidence(profileSearchDto, personData)
+
+            name_linked_data.push({
+              confidence: data,
+              linkedInURL: link
+            })
+          });
+        }
+
+      } catch (error) {
+        console.error(error.message);
+
+      }
+
+    }
+
+
+    if (name_linked_data.length > 0) {
+
+      const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+      for (const data of name_linked_data) {
+        await delay(1200); // wait 1.2 seconds before each call
+
         const options = {
           method: 'POST',
-          url: 'https://linkedin-data-scraper.p.rapidapi.com/email_to_linkedin_profile',
+          url: 'https://linkedin-data-scraper.p.rapidapi.com/person',
           headers: {
             'x-rapidapi-key': 'bcfab707b6msh3b13e094ad1bf26p19071ajsn41f8ae0ee227',
             'x-rapidapi-host': 'linkedin-data-scraper.p.rapidapi.com',
             'Content-Type': 'application/json'
           },
           data: {
-            email: profileSearchDto.email
+            link: data?.linkedInURL
           }
         };
-
         try {
           const response = await axios.request(options);
           if (response?.data?.success) {
 
-            const displayName: string = `${response?.data?.person_data?.displayName}`
-            const companyName: string = `${response?.data?.person_data?.companyName}`
-            const location: string = `${response?.data?.person_data?.location}`
+
+            const companyName = response?.data?.data?.experiences?.map((item: any) => item?.title + " " + item?.subtitle).join(', ')
+
+
+            const displayName: string = `${response?.data?.data?.fullName}`
+
+            const location: string = `${response?.data?.data?.addressWithoutCountry}, ${response?.data?.data?.addressCountryOnly}`
+            const link: string = `https://www.linkedin.com/in/${response?.data?.data?.publicIdentifier}`
 
             const personData: PersonData = {
               displayName,
               companyName,
-              location
+              location,
             }
-            
-            confidence_on_email = this.calculateConfidence(profileSearchDto, personData)
+
+            const data: any = this.calculateConfidence(profileSearchDto, personData)
+
+            url_linked_data.push({
+              confidence: data,
+              linkedInURL: link
+            })
           }
 
-          console.log(response.data);
         } catch (error) {
           console.error(error);
+
         }
+        console.log("url prit")
+
       }
 
-
-    } catch (error) {
-      console.log("error", error)
-      throw new HttpException(error?.message || "An Error Occured", HttpStatus.INTERNAL_SERVER_ERROR)
     }
-  }
-
-  // find linked based on email, google the person on linked in details, find insta, tiktok, etc...
-
-  // async findLinkedProfile(name: string, email: string, city: string, state: string) {
-  async findLinkedProfile() {
-    const name = "Muqeet Ahmad";
-    const email = "engrmuqeetahmad@gmail.com";
-    const city = "Multan";
-    const state = "Punjab";
-    const nameParts = name.trim().split(' ');
-
-
-    const options = {
-      method: 'POST',
-      url: 'https://mtn-email-to-linkedin-lookup-api.p.rapidapi.com/api/email-to-linkedin',
-      headers: {
-        'x-rapidapi-key': '50c23a771dmsh5a551b9a3529d2bp1aa0bdjsn834acd34db7d',
-        'x-rapidapi-host': 'mtn-email-to-linkedin-lookup-api.p.rapidapi.com',
-        'Content-Type': 'application/json'
-      },
-      data: {
-        email: 'engrmuqeetahmad@gmail.com'
-      }
-    };
-
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-
+    console.log("Name", name_linked_data)
+    console.log("Link", url_linked_data)
   }
 }
